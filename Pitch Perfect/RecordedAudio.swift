5ae94a6693d229687c7776bce5512f161febbf61
myslinc <yepes_m@yahoo.com>
2015-04-06 19:32:53 -0500

    Committed by Udacity

---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Michael Yepes on 3/31/15.
+//  Copyright (c) 2015 Michael Yepes. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject {
+    var filePathUrl: NSURL!
+    var title: String!
+    
+}
